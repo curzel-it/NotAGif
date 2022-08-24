e622a84432d70753bea0d7274b9a40f69b104538
Federico Curzel <federico@curzel.it>
2022-08-24 18:39:34 +0200

    Frames provider is now an open class

--- Sources/NotAGif/FramesProvider.swift
+++ Sources/NotAGif/FramesProvider.swift
@@ -4,7 +4,7 @@
 
 import SwiftUI
 
-public struct FramesProvider<T: ResourcesLocator> {
+open class FramesProvider<T: ResourcesLocator> {
     
     let format: String
     let resources: T
@@ -32,9 +32,7 @@ public struct FramesProvider<T: ResourcesLocator> {
         var index = 0
         
         while true {
-            let name = String(format: format, baseName, index)
-            
-            if let url = resources.url(forResource: name, withExtension: fileExtension) {
+            if let url = url(baseName: baseName, index: index) {
                 urls.append(url)
             } else {
                 if index > 0 { break }
@@ -42,6 +40,15 @@ public struct FramesProvider<T: ResourcesLocator> {
             index += 1
         }
         return urls
+    }
+    
+    open func url(baseName: String, index: Int) -> URL? {
+        let name = formatted(baseName: baseName, index: index)
+        return resources.url(forResource: name, withExtension: fileExtension)
+    }
+    
+    func formatted(baseName: String, index: Int) -> String {
+        String(format: format, baseName, index)
     }
 }
 
